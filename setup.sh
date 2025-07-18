5bde3116ffc230f93a21dddf8c306d464c6018d9
thxxx <zxcv05999@naver.com>
2025-07-18 10:36:21 +0000

    load dataset

--- setup.sh
+++ setup.sh
@@ -2,5 +2,10 @@ apt-get update
 git config --global user.email zxcv05999@naver.com
 git config --global user.name thxxx
 python -m pip install --upgrade pip
-pip install tqdm matplotlib einops diffusers accelerate transformers
+pip install tqdm matplotlib einops diffusers accelerate transformers datasets opencv-python torchdiffeq clean-fid jaxtyping tensorboard
+pip install --upgrade pillow
+
+# pip install "numpy<2"
+# pip uninstall torchaudio
+
 # wget https://huggingface.co/CompVis/stable-diffusion-v1-4/resolve/main/vae/diffusion_pytorch_model.safetensors?download=true
